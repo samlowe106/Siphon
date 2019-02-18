faced1bb7b8ae3ca9a4dd8e111a6fd382bbfc62c
jvs8230 <jvs8230@IGMOH1380-12>
2019-02-18 15:46:08 -0500

    Added some inheritance to the classes

--- Siphon/Siphon/Button.cs
+++ Siphon/Siphon/Button.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Siphon
 {
-	class Button
+	class Button : GameObject
 	{
 	}
 }
--- Siphon/Siphon/Player.cs
+++ Siphon/Siphon/Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Siphon
 {
-	class Player
+	class Player : GameObject
 	{
 	}
 }
